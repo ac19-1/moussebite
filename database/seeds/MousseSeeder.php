665e86abf228d76e5440a748c8011bd42e8b2861
ac19-1 <audrey.chen@binus.ac.id>
2020-09-27 20:16:00 +0700

    change assets

--- database/seeds/MousseSeeder.php
+++ database/seeds/MousseSeeder.php
@@ -13,93 +13,93 @@ class MousseSeeder extends Seeder
     public function run()
     {
         $mousse = new Mousse();
-        $mousse->name = 'Cherry Almond Mousse Pie';
-        $mousse->price = 60000;
-        $mousse->stock = 50;
-        $mousse->size_id = 3;
-        $mousse->image = 'Cherry Almond Mousse Pie.jpg';
-        $mousse->description = 'A Mousse Pie with crunchy almond and sweet cherry';
-        $mousse->save();
-
-        $mousse = new Mousse();
-        $mousse->name = 'Creamy Nutella Mousse';
-        $mousse->price = 35000;
-        $mousse->stock = 50;
-        $mousse->size_id = 1;
-        $mousse->image = 'Creamy Nutella Mousse.jpg';
-        $mousse->description = 'A Mousse made with Nutella and fresh strawberries';
-        $mousse->save();
-
-        $mousse = new Mousse();
-        $mousse->name = 'Creamy Raspberry Roll';
-        $mousse->price = 35000;
-        $mousse->stock = 50;
-        $mousse->size_id = 3;
-        $mousse->image = 'Creamy Raspberry Roll.jpg';
-        $mousse->description = 'Cream filled wafer roll and peanut squares garnished with fresh raspberries';
-        $mousse->save();
-
-        $mousse = new Mousse();
-        $mousse->name = 'Double Layered Mix Berries Mousse';
-        $mousse->price = 25000;
-        $mousse->stock = 50;
-        $mousse->size_id = 2;
-        $mousse->image = 'Double Layered Mix Berries Mousse.jpg';
-        $mousse->description = 'Two-layered mousse with fresh blackberry and raspberry';
-        $mousse->save();
-
-        $mousse = new Mousse();
-        $mousse->name = 'Layered Fruit Mousse';
+        $mousse->name = 'Choco Strawberry Almond Mousse';
         $mousse->price = 30000;
         $mousse->stock = 50;
-        $mousse->size_id = 2;
-        $mousse->image = 'Layered Fruit Mousse.jpg';
-        $mousse->description = 'Mix fruit layered mousse with fresh fruits and cream';
+        $mousse->size_id = 3;
+        $mousse->image = 'Choco Strawberry Almond Mousse.png';
+        $mousse->description = 'Chocolate mousse with crunchy almonds and fresh strawberries';
         $mousse->save();
 
         $mousse = new Mousse();
-        $mousse->name = 'Lemon Cheesecake Mousse';
+        $mousse->name = 'Chocolate Almond Mousse';
+        $mousse->price = 25000;
+        $mousse->stock = 50;
+        $mousse->size_id = 1;
+        $mousse->image = 'Chocolate Almond Mousse.png';
+        $mousse->description = 'Chocolate mousse with crunchy almonds';
+        $mousse->save();
+
+        $mousse = new Mousse();
+        $mousse->name = 'Double Chocolate Mousse';
         $mousse->price = 25000;
         $mousse->stock = 50;
         $mousse->size_id = 3;
-        $mousse->image = 'Lemon Cheesecake Mousse.jpg';
-        $mousse->description = 'Cheesecake mousse with fresh lemon slice';
+        $mousse->image = 'Double Chocolate Mousse.png';
+        $mousse->description = 'Chocolate mousse with sweet chocolate balls';
         $mousse->save();
 
         $mousse = new Mousse();
-        $mousse->name = 'Light Raspberry Mousse';
+        $mousse->name = 'Peanut Butter Mousse';
+        $mousse->price = 25000;
+        $mousse->stock = 50;
+        $mousse->size_id = 2;
+        $mousse->image = 'Peanut Butter Mousse.png';
+        $mousse->description = 'Peanut butter mousse with sweet chocolate balls';
+        $mousse->save();
+
+        $mousse = new Mousse();
+        $mousse->name = 'Strawberry Almond Mousse';
+        $mousse->price = 25000;
+        $mousse->stock = 50;
+        $mousse->size_id = 2;
+        $mousse->image = 'Strawberry Almond Mousse.png';
+        $mousse->description = 'Strawberry mousse with crunchy almonds';
+        $mousse->save();
+
+        $mousse = new Mousse();
+        $mousse->name = 'Strawberry Mousse';
         $mousse->price = 20000;
         $mousse->stock = 50;
         $mousse->size_id = 3;
-        $mousse->image = 'Light Raspberry Mousse.jpg';
-        $mousse->description = 'Raspberry mousse with fresh raspberry';
+        $mousse->image = 'Strawberry Mousse.png';
+        $mousse->description = 'Strawberry mousse with fresh strawberry slices';
         $mousse->save();
 
         $mousse = new Mousse();
-        $mousse->name = 'Strawberry White Choco Mousse';
-        $mousse->price = 30000;
-        $mousse->stock = 50;
-        $mousse->size_id = 1;
-        $mousse->image = 'Strawberry White Choco Mousse.jpg';
-        $mousse->description = 'Three-layered mousse with sweet white chocolate and fresh strawberries';
-        $mousse->save();
-
-        $mousse = new Mousse();
-        $mousse->name = 'Triple Chocolate Mousse';
-        $mousse->price = 30000;
-        $mousse->stock = 50;
-        $mousse->size_id = 1;
-        $mousse->image = 'Triple Chocolate Mousse.jpg';
-        $mousse->description = 'Triple chocolate mousse with cream and cocoa powder';
-        $mousse->save();
-
-        $mousse = new Mousse();
-        $mousse->name = 'Two Tone Chocolate Mousse';
-        $mousse->price = 20000;
+        $mousse->name = 'Strawberry Yoghurt Mousse';
+        $mousse->price = 25000;
         $mousse->stock = 50;
         $mousse->size_id = 3;
-        $mousse->image = 'Two Tone Chocolate Mousse.jpg';
-        $mousse->description = 'Double chocolate mousse with white chocolate';
+        $mousse->image = 'Strawberry Yoghurt Mousse.png';
+        $mousse->description = 'Strawberry mousse mixed with fresh yoghurt with sweet strawberries';
+        $mousse->save();
+
+        $mousse = new Mousse();
+        $mousse->name = 'Tri-layered Almond Mousse';
+        $mousse->price = 30000;
+        $mousse->stock = 50;
+        $mousse->size_id = 1;
+        $mousse->image = 'Tri-layered Almond Mousse.png';
+        $mousse->description = 'Tri-layered mousse with crunchy almonds';
+        $mousse->save();
+
+        $mousse = new Mousse();
+        $mousse->name = 'Tri-layered Raspberry Mousse';
+        $mousse->price = 30000;
+        $mousse->stock = 50;
+        $mousse->size_id = 1;
+        $mousse->image = 'Tri-layered Raspberry Mousse.png';
+        $mousse->description = 'Tri-layered mousse with fresh and sweet raspberries';
+        $mousse->save();
+
+        $mousse = new Mousse();
+        $mousse->name = 'Tri-layered Strawberry Mousse';
+        $mousse->price = 30000;
+        $mousse->stock = 50;
+        $mousse->size_id = 3;
+        $mousse->image = 'Tri-layered Strawberry Mousse.png';
+        $mousse->description = 'Tri-layered mousse with fresh and sweet strawberries';
         $mousse->save();
     }
 }
